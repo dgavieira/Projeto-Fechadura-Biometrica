c1a8f9137faebceeacebf496bbaae840eee4f570
Leonardo Arcanjo <leonardoarcanjoo@gmail.com>
2020-01-10 14:53:01 -0400

    Tentando fazer a tela do Acess.py

--- User-Interface/tela10.py
+++ User-Interface/tela10.py
@@ -14,7 +14,10 @@ except:
     #for Python3
     from tkinter import *
     
+from pyfingerprint.pyfingerprint import PyFingerprint
+import time
 import tela01
+
 
 def teladez():
     class ScreenTen:
@@ -22,7 +25,7 @@ def teladez():
             self.widget1 = Frame(master)
             self.widget1["pady"] = 10
             self.widget1.pack()
-            
+ 
             self.widget2 = Frame(master)
             self.widget2["padx"] = 10
             self.widget2.pack()
@@ -36,7 +39,50 @@ def teladez():
             self.texto["height"] = 13
             self.texto.pack()
             
+            self.texto.insert(END, "Initializing the sensor\n")
             
+            self.connectSensor()
+            
+        def connectSensor(self):            
+            try:
+                f = PyFingerprint('/dev/ttyUSB0', 57600, 0xFFFFFFFF, 0x00000000)
+                msg = "Sensor Fingerprint Connected\n"
+                self.texto.insert(END, msg)
+
+            except Exception as e:
+                msg = "Sensor Fingerprint could not be initialized!!!\nError: " + str(e) + "\n"
+                self.texto.insert(END, msg)
+            
+            """try:
+                msg = 'Waiting for finger...\n'
+                self.texto.insert(END, msg)
+                
+                while(f.readImage() == False):
+                    pass
+                
+                f.convertImage(0x01)
+                
+                result = f.searchTemplate()
+                
+                positionIndex = result[0]
+                
+                if (positionIndex == -1):
+                    msg = 'No match found!!!\n'
+                    self.texto.insert(END, msg)
+                else:
+                    msg = 'Found template at position #'+ str(positionIndex)+"\n"
+                    self.texto.insert(END, msg)
+                    #DBAcess(positionIndex)
+                    #writeDatalog()
+                    #fechar()
+            except Exception as e:
+                msg = 'Operation Failed!!!\nError: '+ str(e)+'\n'
+                #self.texto.insert(END, msg)"""
+    
+    def fechar():
+        root.destroy()
+        tela01.telaum()
+    
     root = Tk()
     ScreenTen(root)
     root.title("Check-in Screen")
