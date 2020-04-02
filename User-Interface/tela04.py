6a186ce77506c97005cc65726ebeacb64497d77e
Leonardo Arcanjo <leonardoarcanjoo@gmail.com>
2020-04-01 21:23:21 -0400

    Fix bugs on Tela04.py"

--- User-Interface/tela04.py
+++ User-Interface/tela04.py
@@ -92,8 +92,7 @@ def telaquatro():
             self.check["font"] = fontePadrao
             self.check["text"] = "Admin"
             self.check["variable"] = self.var
-            self.check.grid(row = 2, column = 2, padx = 0, pady = 0, sticky = NE)
-            
+            self.check.grid(row = 2, column = 2, padx = 0, pady = 0, sticky = NW)
             
             self.botaoLoad = Button(self.primeiroContainer)
             self.botaoLoad["text"] = "LOAD"
@@ -116,7 +115,7 @@ def telaquatro():
             
             self.botaoCancel = Button(self.primeiroContainer)
             self.botaoCancel["text"] = "CANCEL"
-            self.botaoCancel["width"] = 15
+            self.botaoCancel["width"] = 24
             self.botaoCancel["font"] = fontePadrao
             self.botaoCancel["command"] = self.eraseinput
             self.botaoCancel.grid(row = 6, column = 1, padx = 0, pady = 0, sticky = NW)
@@ -135,6 +134,7 @@ def telaquatro():
             p_first_name = self.firstname.get()
             p_last_name = self.lastname.get()
             p_title = self.title.get()
+            p_password = self.password.get()
             p_admin = self.var.get()
 
             #troca texto para string com dados adicionados
@@ -154,28 +154,30 @@ def telaquatro():
             p_first_name = self.firstname.get()
             p_last_name = self.lastname.get()
             p_title = self.title.get()
+            p_password = self.password.get()
             p_admin = self.var.get()
 
             # apaga o que foi mostrado no widget msg
-
-            if self.msg["text"] == "First Name: " + p_first_name + "\n Last Name: " + p_last_name + "\n Title: " + p_title + "\n Admin: YES":
+            if((p_first_name != "") and (p_last_name != "") and (p_title != "") and (p_admin == 1)):
                 self.msg["text"] = "First Name: \n Last Name: \n Title: \n Admin:"
                 self.botaoLoad["state"] = NORMAL
                 self.check.toggle()
                 self.firstname.delete(0,END)
                 self.lastname.delete(0,END)
                 self.title.delete(0,END)
+                self.password.delete(0,END)
                 del(p_first_name)
                 del(p_last_name)
                 del(p_title)
                 del(p_admin)
                 self.botaoFingerprint["state"] = NORMAL
-            elif self.msg["text"] == "First Name: \t" + p_first_name + "\n Last Name: \t" + p_last_name + "\n Title: \t" + p_title + "\n Admin: NO":
+            elif ((p_first_name != "") and (p_last_name != "") and (p_title != "") and (p_admin == 0)):
                 self.msg["text"] = "First Name: \n Last Name: \n Title: \n Admin:"
                 self.botaoLoad["state"] = NORMAL
                 self.firstname.delete(0,END)
                 self.lastname.delete(0,END)
                 self.title.delete(0,END)
+                self.password.delete(0,END)
                 del(p_first_name)
                 del(p_last_name)
                 del(p_title)
@@ -190,6 +192,7 @@ def telaquatro():
             p_first_name = self.firstname.get()
             p_last_name = self.lastname.get()
             p_title = self.title.get()
+            p_password = self.password.get()
             p_admin = self.var.get()
             
             # tratamento de excecao para verificacao de duplicata
@@ -197,16 +200,15 @@ def telaquatro():
             # converte as strings para minusculas
             pfirstname = p_first_name.casefold()
             plastname = p_last_name.casefold()
-            ptitle = p_title.casefold()
-
+            
             try:
                 #escreve valores no banco
                 conn = sqlite3.connect('optima.db')
                 cursor = conn.cursor()
                 cursor.execute("""
-                    INSERT INTO optima (first_name, last_name, title, admin)
-                    VALUES (?, ?, ?, ?)
-                    """, (pfirstname, plastname, ptitle, p_admin)
+                    INSERT INTO optima (first_name, last_name, title, admin, senhas)
+                    VALUES (?, ?, ?, ?, ?)
+                    """, (pfirstname, plastname, ptitle, p_admin, p_password)
                     )
                 conn.commit()
                 conn.close()
@@ -236,35 +238,30 @@ def telaquatro():
                 tela05.telacinco()
                 
             except: #se o python levantar uma excecao - ocorre esse loop
-                if self.msg["text"] == "First Name: " + p_first_name + "\n Last Name: " + p_last_name + "\n Title: " + p_title + "\n Admin: YES":
+                if ((p_first_name != "") and (p_last_name != "") and (p_title != "") and (p_admin == 1) or (p_admin == 0)):
                     self.msg["text"] = "Name already enrolled. Input new data."
                     self.botaoLoad["state"] = DISABLED
                     self.botaoFingerprint["state"] = DISABLED
-                    self.check.toggle()
+                    
+                    if p_admin == 1:
+                        self.check.toggle()
+                    
                     self.firstname.delete(0,END)
                     self.lastname.delete(0,END)
                     self.title.delete(0,END)
+                    self.password.delete(0,END)
+                    
                     del(p_first_name)
                     del(p_last_name)
                     del(p_title)
+                    del(p_password)
                     del(p_admin)
-                    self.botaoFingerprint["state"] = NORMAL
-                    self.botaoLoad["state"] = NORMAL
-                elif self.msg["text"] == "First Name: " + p_first_name + "\n Last Name: " + p_last_name + "\n Title: " + p_title + "\n Admin: NO":
-                    self.msg["text"] = "Name already enrolled. Input new data."
-                    self.botaoLoad["state"] = DISABLED
-                    self.botaoFingerprint["state"] = DISABLED
-                    self.firstname.delete(0,END)
-                    self.lastname.delete(0,END)
-                    self.title.delete(0,END)
-                    del(p_first_name)
-                    del(p_last_name)
-                    del(p_title)
-                    del(p_admin)
+                    
                     self.botaoFingerprint["state"] = NORMAL
                     self.botaoLoad["state"] = NORMAL
                 else:
                     self.msg["text"] = "First Name: \n Last Name: \n Title: \n Admin:"
+                    
                     
     def returntohome(): #metodo de retornar a tela principal
         fechar()
